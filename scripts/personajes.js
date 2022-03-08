ecd495419ac87906174c126285df69eb79b609f8
Andreina Rodriguez <andreaguerrero.ar@gmail.com>
2022-03-08 09:25:14 -0500

    se suben cambios de formulario

--- scripts/personajes.js
+++
@@ -1,7 +0,0 @@
-let formulario = document.getElementById('formulario')
-let btnCorreo = document.getElementById('btnCorreo')
-let btnEditar = document.getElementById('btnEditar')
-let btnEliminar = document.getElementById('btnEliminar')
-let url =''
-
-document
